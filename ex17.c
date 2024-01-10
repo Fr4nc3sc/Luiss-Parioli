6cd3a90fe2ae3d60d6e2e0388903aa0ebe0d2530
Foxins7 <62253322+Fr4nc3sc@users.noreply.github.com>
2024-01-10 11:54:08 +0100

    Add files via upload

---
+++ ex17.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+void ft_interative_power(int nb, int power)
+{
+    int res;
+    res = nb;
+    if(power < 0){
+      printf("%d", 0);
+    }
+    if(power == 0){
+      printf("%d", 1);
+    }
+    while(power --> 1){
+        res *= nb;
+    }
+    printf("%d", res);
+    
+}
+int main ()
+{
+    int nb, power;
+    printf("Inserisci un numero\n");
+    scanf("%d %d", &nb, &power);
+    ft_interative_power(nb, power);
+    return (0);
+}
